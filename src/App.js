97aa442c012736aceabe0a4d01fed48741b103d0
Cristopher Bechtholdt <cristopherbechtholdt@gmail.com>
2024-06-22 16:12:18 -0300

    Set dark and light theme

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import AppTemplate from './template/AppTemplate';
 import AppRoutes from './routes/AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
-import theme from './utils/theme';
+import { useMemo, useState } from 'react';
+import getDesignToken from './utils/theme';
+import { createTheme } from '@mui/material';
 
 function App() {
+  const [mode, setMode] = useState('dark')
+  const colorMode = useMemo(() => ({
+    toggleColorMode: () => {
+      setMode((prevMode) => prevMode === 'light' ? 'dark': 'light')
+    }
+  }))
+
+  const theme = useMemo(() => createTheme(getDesignToken(mode)), [mode])
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,46 +1,46 @@
-import { Padding } from '@mui/icons-material';
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-        main: '#4d1f47',
-    },
-    secondary: {
-        main: '#FFD9E1'
-    }
-  },
-  components: {
-    MuiBottomNavigationAction: {
-        styleOverrides: {
-            label: ({theme}) => ({
-                color: theme.palette.secondary.main
-            }),
-            root: ({theme}) => ({
-                '&.Mui-selected .MuiSvgIcon-root': {
-                    backgroundColor: theme.palette.primary.light,
-                    paddingLeft: 8,
-                    paddingRight: 8,
-                    borderRadius: 12
-                }
-            }),
+const getDesignToken = (mode) => ({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#702b67',
         },
-    },
-    MuiBottomNavigation: {
-        styleOverrides: {
-            root: ({theme}) => ({
-                position: "absolute",
-                bottom: "16px",
-                borderRadius: 16,
-                maxWidth: "40%",
-                margin: "0 auto",
-                left: 0,
-                right: 0,
-                background: theme.palette.background
-            })}
+        secondary: {
+            main: '#FFD9E1'
+        },
+        background: {
+            default: mode === 'light' ? '#eddded' : '#423741'
         }
-    }
+    },
+    components: {
+        MuiBottomNavigationAction: {
+            styleOverrides: {
+                label: ({theme}) => ({
+                    color: theme.palette.secondary.main
+                }),
+                root: ({theme}) => ({
+                    '&.Mui-selected .MuiSvgIcon-root': {
+                        backgroundColor: theme.palette.primary.main,
+                        paddingLeft: 8,
+                        paddingRight: 8,
+                        borderRadius: 12
+                    }
+                }),
+            },
+        },
+        MuiBottomNavigation: {
+            styleOverrides: {
+                root: ({theme}) => ({
+                    position: "absolute",
+                    bottom: "16px",
+                    borderRadius: 16,
+                    maxWidth: "40%",
+                    margin: "0 auto",
+                    left: 0,
+                    right: 0,
+                    background: theme.palette.background.default
+                })}
+            }
+        }
 });
 
-export default theme;
+export default getDesignToken;
