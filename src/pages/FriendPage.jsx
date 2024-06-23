d92be64736cdea7addbb6e6549439c8656ae75f3
agutierrez <agutierrez@zetti.com.ar>
2024-06-22 23:16:29 -0300

    autenticacion con google auth de firebase

--- src/pages/FriendPage.jsx
+++ src/pages/FriendPage.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { auth, provider } from "../configs/firebase";
+import { signInWithPopup } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const FriendPage = () => {
+  const [user] = useAuthState(auth);
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+  };
+
   return (
-    <div>FriendPage</div>
-  )
+    <>
+      <h1>FriendPage</h1>
+      {user ? (
+        <>
+          <h1>Hello, {user?.displayName}</h1>
+          <button onClick={handleLogout}>Log out</button>
+        </>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign In with Google</button>
+      )}
+    </>
+  );
 }
 
 export default FriendPage
