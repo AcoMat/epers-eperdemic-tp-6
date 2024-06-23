e4e37a475017a5f6462b26c1ef298785d62907bf
agutierrez <agutierrez@zetti.com.ar>
2024-06-23 10:58:59 -0300

    relacion vector-usuario en firebase realtime

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider();
+const dbFs = getFirestore(app);
+const dbRt = getDatabase();
 
-export { auth, provider };
+export { auth, provider, dbFs, dbRt };
--- src/pages/FriendPage.jsx
+++ src/pages/FriendPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { auth, provider } from "../configs/firebase";
+import React, { useEffect, useState } from 'react'
+import { auth, provider, dbFs, dbRt } from "../configs/firebase";
 import { signInWithPopup } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+import { createVector, getVector } from '../api/api';
+import { ref, set } from 'firebase/database';
 
 const FriendPage = () => {
   const [user] = useAuthState(auth);
+  const [userVector, setUserVector] = useState()
+  const [vectorToUser, setVectorToUser] = useState()
 
   const signInWithGoogle = async () => {
     try {
@@ -13,6 +18,48 @@ const FriendPage = () => {
       console.error("Error signing in with Google:", error);
     }
   };
+
+  const crearVector = async () => {
+    try {
+      const vector = await createVector({ type: 'HUMANO', ubicacionId: '1' })
+      console.log(vector)
+      setUserVector(vector)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const recuperarVector = async () => {
+    try {
+      const vector = await getVector({ vectorId: 1 })
+      console.log(vector)
+      setUserVector(vector)
+
+      const usuarioVector = {
+        displayName: user.displayName,
+        email: user.email,
+        vector: {
+          id: vector.id,
+          tipoVector: vector.tipoVector,
+        }
+      };
+      
+      setVectorToUser(usuarioVector)
+      console.log(usuarioVector)
+
+      set(ref(dbRt, 'users/usuariodbrt'), usuarioVector)
+
+      // setDoc(doc(dbFs, "users", "usuariodbfs"), usuarioVector)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      recuperarVector()
+    }
+  }, [user])
 
   const handleLogout = () => {
     auth.signOut();
